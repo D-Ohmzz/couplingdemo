0087d0fe3449e77d6108354c9ab58f8b4236d77a
D-Ohmzz <douglasomega13@gmail.com>
2025-06-19 11:27:00 +0300

    dependee class- car class needs its object

---
+++ src/main/java/car/example/setter/injection/Specification.java
@@ -0,0 +1,33 @@
+package car.example.setter.injection;
+
+public class Specification {
+    private String make;
+    private String model;
+
+    public void setMake(String make){
+        this.make=make;
+    }
+    public String getMake(){
+        return make;
+    }
+
+    public void setModel(String model){
+        this.model=model;
+    }
+    public String getModel(){
+        return model;
+    }
+
+    @Override
+    public String toString(){
+        return "Specification{" + 
+                "make= '" + make + '\'' +
+                ", model= '" + model + '\'' +
+                '}';
+
+    }
+
+
+
+
+}
